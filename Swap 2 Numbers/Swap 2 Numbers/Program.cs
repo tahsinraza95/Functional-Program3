04d07c9ec1d67bfb55372af76ec6373236a53737
tahsinraza95 <tahsinr693@gmail.com>
2023-02-06 05:16:03 +0530

    Updated: Numbers

---
+++ Swap 2 Numbers/Swap 2 Numbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Swap_2_Numbers
+{
+    internal class Program
+        //Swaping of Two NUmber
+    {
+        static void Main(string[] args)
+        {
+            int val1, val2;
+            val1 = 100;
+            val2 = 200;
+            Console.WriteLine("Values before swap");
+            Console.WriteLine(val1.ToString());
+            Console.WriteLine(val2.ToString());
+
+            val1 = val1 + val2;
+            val2 = val1 - val2;
+            val1 = val1 - val2;
+            Console.WriteLine("Values after swap");
+            Console.WriteLine(val1.ToString());
+            Console.WriteLine(val2.ToString());
+            Console.ReadLine();
+        }
+    }
+}
